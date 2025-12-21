a2f824c61a2a3dee86ba51640410abcc45b7ec55
ragoog <ragiasidahmed212005@gmail.com>
2025-12-21 18:17:01 +0200

    GUI skeleton

---
+++ Plant Feed GUI/PlantFeedGUI/src/main/java/com/example/plantfeedgui/FeedPageController.java
@@ -0,0 +1,17 @@
+package com.example.plantfeedgui;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+public class FeedPageController {
+
+        @FXML
+        private VBox postsContainer;
+
+        @FXML
+        private TextField postTextField;
+
+        @FXML
+        private Button postButton;
+    }
