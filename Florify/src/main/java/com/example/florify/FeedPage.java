4cbfa54e7901c5a1cd00e21d5a709632056f4d07
ragoog <ragiasidahmed212005@gmail.com>
2025-12-27 12:16:44 +0200

    Feed Page

---
+++ Florify/src/main/java/com/example/florify/FeedPage.java
@@ -0,0 +1,16 @@
+package com.example.florify;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class FeedPage extends Application
+{
+    public static  void main(String[] args)
+    {
+        launch(args);
+    }
+
+    public void  start(Stage stage)
+    {
+    }
+}
